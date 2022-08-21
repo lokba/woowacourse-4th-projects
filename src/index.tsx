e33b70b49679345db3376fdb18ef3d90e821a2f8
kimsanglok <evertree6031@gmail.com>
2022-08-21 18:10:35 +0900

    chore: typescript 환경 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 root.render(
   <React.StrictMode>
     <App />
