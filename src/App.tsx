a7d3b632a4f27d3cd8dfc5805b0e695dce308f4e
kimsanglok <evertree6031@gmail.com>
2022-08-22 00:40:47 +0900

    refactor: 불필요한 폴더 삭제 및 import문 정렬

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import Header from "components/Header";
 import { ThemeProvider } from "styled-components";
+
 import GlobalStyle from "styles/GlobalStyle";
 import theme from "styles/theme";
+
+import HomePage from "pages/HomePage";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
+      <HomePage />
     </ThemeProvider>
   );
 };
