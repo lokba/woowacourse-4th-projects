32c989810e20a2546a181e2340e71b02eebdd27c
kimsanglok <evertree6031@gmail.com>
2022-08-22 01:35:49 +0900

    feat: theme에 디바이스 크기에 따른 미디어 쿼리문 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,9 +8,20 @@ const theme = {
   shadows: {
     basic: "#64646f33 0px 7px 29px 0px",
   },
+  devices: {
+    mobileS: (adjustment: number = 0) => `(max-width: ${320 + adjustment}px)`,
+    mobileM: (adjustment: number = 0) => `(max-width: ${375 + adjustment}px)`,
+    mobileL: (adjustment: number = 0) => `(max-width: ${425 + adjustment}px)`,
+    tabletM: (adjustment: number = 0) => `(max-width: ${590 + adjustment}px)`,
+    tablet: (adjustment: number = 0) => `(max-width: ${768 + adjustment}px)`,
+    laptop: (adjustment: number = 0) => `(max-width: ${1024 + adjustment}px)`,
+    laptopL: (adjustment: number = 0) => `(max-width: ${1440 + adjustment}px)`,
+    desktop: (adjustment: number = 0) => `(max-width: ${2560 + adjustment}px)`,
+  },
 };
 
 export type Colors = typeof theme.colors;
 export type Shadows = typeof theme.shadows;
+export type Devices = typeof theme.devices;
 
 export default theme;
--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,10 +1,11 @@
 import "styled-components";
 
-import { Colors, Shadows } from "styles/theme";
+import { Colors, Devices, Shadows } from "styles/theme";
 
 declare module "styled-components" {
   export interface DefaultTheme {
     colors: Colors;
     shadows: Shadows;
+    devices: Devices;
   }
 }
